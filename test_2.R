4863bd69ea0a68789f036f1fe464887dabcb3ef9
unknown <L00169749@student.lyit.ie>
2022-03-10 12:30:01 +0000

    logical code

---
+++ test_2.R
@@ -0,0 +1,19 @@
+1:20
+20
+pi:11
+pi
+15:1
+seq(1, 20, 0.5)
+seq(1, 10, length=30)
+rep(0,time=5)
+
+rep(z,time=9)
+p <- c(0, 1, 2)
+rep( p, each = 10)
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect >= 6
+
+txt <- c("my","name","is")
+txy <- c("yesh")
+text_fin <- paste(c(txt, txy),collapse = " ")
+text_fin
